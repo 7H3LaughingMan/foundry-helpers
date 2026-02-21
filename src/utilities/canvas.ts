c1cc603c8e04a822ed3787f99d48d079f8c70de1
7H3LaughingMan <7H3LaughingMan@proton.me>
2026-02-20 18:44:56 -0600

    More Stuff

--- src/utilities/canvas.ts
+++ src/utilities/canvas.ts
@@ -1,18 +1,38 @@
-type PlaceableObjectMap = {
-    AmbientLight: typeof foundry.canvas.placeables.AmbientLight;
-    AmbientSound: typeof foundry.canvas.placeables.AmbientSound;
-    Drawing: typeof foundry.canvas.placeables.Drawing;
-    MeasuredTemplate: typeof foundry.canvas.placeables.MeasuredTemplate;
-    Note: typeof foundry.canvas.placeables.Note;
-    Region: typeof foundry.canvas.placeables.Region;
-    Tile: typeof foundry.canvas.placeables.Tile;
-    Token: typeof foundry.canvas.placeables.Token;
-    Wall: typeof foundry.canvas.placeables.Wall;
-};
+import * as R from "remeda";
 
-export function isPlaceableObject<K extends keyof PlaceableObjectMap>(value: unknown, type: K): value is PlaceableObjectMap[K] {
-    return (
-        value instanceof foundry.canvas.placeables.PlaceableObject &&
-        (value.constructor as typeof foundry.canvas.placeables.PlaceableObject).embeddedName === type
-    );
+import AmbientLight = foundry.canvas.placeables.AmbientLight;
+import AmbientSound = foundry.canvas.placeables.AmbientSound;
+import Drawing = foundry.canvas.placeables.Drawing;
+import MeasuredTemplate = foundry.canvas.placeables.MeasuredTemplate;
+import Note = foundry.canvas.placeables.Note;
+import PlaceableObject = foundry.canvas.placeables.PlaceableObject;
+import Region = foundry.canvas.placeables.Region;
+import Tile = foundry.canvas.placeables.Tile;
+import Token = foundry.canvas.placeables.Token;
+import Wall = foundry.canvas.placeables.Wall;
+
+export function isPlaceableObject(value: unknown, embeddedName: "AmbientLight"): value is AmbientLight;
+export function isPlaceableObject(value: unknown, embeddedName: "AmbientSound"): value is AmbientSound;
+export function isPlaceableObject(value: unknown, embeddedName: "Drawing"): value is Drawing;
+export function isPlaceableObject(value: unknown, embeddedName: "MeasuredTemplate"): value is MeasuredTemplate;
+export function isPlaceableObject(value: unknown, embeddedName: "Note"): value is Note;
+export function isPlaceableObject(value: unknown, embeddedName: "Region"): value is Region;
+export function isPlaceableObject(value: unknown, embeddedName: "Tile"): value is Tile;
+export function isPlaceableObject(value: unknown, embeddedName: "Token"): value is Token;
+export function isPlaceableObject(value: unknown, embeddedName: "Wall"): value is Wall;
+export function isPlaceableObject(value: unknown, embeddedName: string): boolean {
+    return value instanceof PlaceableObject && (value.constructor as typeof PlaceableObject).embeddedName === embeddedName;
+}
+
+export function placeableObjectHasScene(value: Maybe<AmbientLight>): value is AmbientLight<AmbientLightDocument<Scene>>;
+export function placeableObjectHasScene(value: Maybe<AmbientSound>): value is AmbientSound<AmbientSoundDocument<Scene>>;
+export function placeableObjectHasScene(value: Maybe<Drawing>): value is Drawing<DrawingDocument<Scene>>;
+export function placeableObjectHasScene(value: Maybe<MeasuredTemplate>): value is MeasuredTemplate<MeasuredTemplateDocument<Scene>>;
+export function placeableObjectHasScene(value: Maybe<Note>): value is Note<NoteDocument<Scene>>;
+export function placeableObjectHasScene(value: Maybe<Region>): value is Region<RegionDocument<Scene>>;
+export function placeableObjectHasScene(value: Maybe<Tile>): value is Tile<TileDocument<Scene>>;
+export function placeableObjectHasScene(value: Maybe<Token>): value is Token<TokenDocument<Scene>>;
+export function placeableObjectHasScene(value: Maybe<Wall>): value is Wall<WallDocument<Scene>>;
+export function placeableObjectHasScene(value: Maybe<PlaceableObject>): boolean {
+    return R.isNonNullish(value?.scene);
 }
--- src/utilities/document.ts
+++ src/utilities/document.ts
@@ -1,3 +1,68 @@
-export function isDocumentType<K extends keyof DocumentTypeMap>(value: unknown, name: K): value is DocumentTypeMap[K] {
-    return value instanceof getDocumentClass(name);
+import * as R from "remeda";
+
+import Document = foundry.abstract.Document;
+
+export function isDocumentType(value: unknown, documentName: "ActiveEffect"): value is ActiveEffect;
+export function isDocumentType(value: unknown, documentName: "Actor"): value is Actor;
+export function isDocumentType(value: unknown, documentName: "ActorDelta"): value is ActorDelta;
+export function isDocumentType(value: unknown, documentName: "Adventure"): value is Adventure;
+export function isDocumentType(value: unknown, documentName: "AmbientLight"): value is AmbientLightDocument;
+export function isDocumentType(value: unknown, documentName: "AmbientSound"): value is AmbientSoundDocument;
+export function isDocumentType(value: unknown, documentName: "Card"): value is Card;
+export function isDocumentType(value: unknown, documentName: "Cards"): value is Cards;
+export function isDocumentType(value: unknown, documentName: "ChatMessage"): value is ChatMessage;
+export function isDocumentType(value: unknown, documentName: "Combat"): value is Combat;
+export function isDocumentType(value: unknown, documentName: "Combatant"): value is Combatant;
+export function isDocumentType(value: unknown, documentName: "CombatantGroup"): value is CombatantGroup;
+export function isDocumentType(value: unknown, documentName: "Drawing"): value is DrawingDocument;
+export function isDocumentType(value: unknown, documentName: "FogExploration"): value is FogExploration;
+export function isDocumentType(value: unknown, documentName: "Folder"): value is Folder;
+export function isDocumentType(value: unknown, documentName: "Item"): value is Item;
+export function isDocumentType(value: unknown, documentName: "JournalEntry"): value is JournalEntry;
+export function isDocumentType(value: unknown, documentName: "JournalEntryCategory"): value is JournalEntryCategory;
+export function isDocumentType(value: unknown, documentName: "JournalEntryPage"): value is JournalEntryPage;
+export function isDocumentType(value: unknown, documentName: "Macro"): value is Macro;
+export function isDocumentType(value: unknown, documentName: "MeasuredTemplate"): value is MeasuredTemplateDocument;
+export function isDocumentType(value: unknown, documentName: "Note"): value is NoteDocument;
+export function isDocumentType(value: unknown, documentName: "Playlist"): value is Playlist;
+export function isDocumentType(value: unknown, documentName: "PlaylistSound"): value is PlaylistSound;
+export function isDocumentType(value: unknown, documentName: "Region"): value is RegionDocument;
+export function isDocumentType(value: unknown, documentName: "RegionBehavior"): value is RegionBehavior;
+export function isDocumentType(value: unknown, documentName: "RollTable"): value is RollTable;
+export function isDocumentType(value: unknown, documentName: "Scene"): value is Scene;
+export function isDocumentType(value: unknown, documentName: "Setting"): value is Setting;
+export function isDocumentType(value: unknown, documentName: "TableResult"): value is TableResult;
+export function isDocumentType(value: unknown, documentName: "Tile"): value is TileDocument;
+export function isDocumentType(value: unknown, documentName: "Token"): value is TokenDocument;
+export function isDocumentType(value: unknown, documentName: "User"): value is User;
+export function isDocumentType(value: unknown, documentName: "Wall"): value is WallDocument;
+export function isDocumentType(value: unknown, documentName: string): boolean {
+    return value instanceof Document && value.documentName === documentName;
+}
+
+export function documentHasParent(value: Maybe<ActiveEffect>): value is ActiveEffect<Actor | Item>;
+export function documentHasParent(value: Maybe<Actor>): value is Actor<TokenDocument>;
+export function documentHasParent(value: Maybe<ActorDelta>): value is ActorDelta<TokenDocument>;
+export function documentHasParent(value: Maybe<AmbientLightDocument>): value is AmbientLightDocument<Scene>;
+export function documentHasParent(value: Maybe<AmbientSoundDocument>): value is AmbientSoundDocument<Scene>;
+// @ts-expect-error Type instantiation is excessively deep and possibly infinite
+export function documentHasParent(value: Maybe<Card>): value is Card<Cards>;
+export function documentHasParent(value: Maybe<ChatMessage>): value is ChatMessage<User>;
+export function documentHasParent(value: Maybe<Combatant>): value is Combatant<Combat>;
+export function documentHasParent(value: Maybe<CombatantGroup>): value is CombatantGroup<Combat>;
+export function documentHasParent(value: Maybe<DrawingDocument>): value is DrawingDocument<Scene>;
+export function documentHasParent(value: Maybe<Item>): value is Item<Actor>;
+export function documentHasParent(value: Maybe<JournalEntryCategory>): value is JournalEntryCategory<JournalEntry>;
+export function documentHasParent(value: Maybe<JournalEntryPage>): value is JournalEntryPage<JournalEntry>;
+export function documentHasParent(value: Maybe<MeasuredTemplateDocument>): value is MeasuredTemplateDocument<Scene>;
+export function documentHasParent(value: Maybe<NoteDocument>): value is NoteDocument<Scene>;
+export function documentHasParent(value: Maybe<PlaylistSound>): value is PlaylistSound<Playlist>;
+export function documentHasParent(value: Maybe<RegionDocument>): value is RegionDocument<Scene>;
+export function documentHasParent(value: Maybe<RegionBehavior>): value is RegionBehavior<RegionDocument>;
+export function documentHasParent(value: Maybe<TableResult>): value is TableResult<RollTable>;
+export function documentHasParent(value: Maybe<TileDocument>): value is TileDocument<Scene>;
+export function documentHasParent(value: Maybe<TokenDocument>): value is TokenDocument<Scene>;
+export function documentHasParent(value: Maybe<WallDocument>): value is WallDocument<Scene>;
+export function documentHasParent(value: Maybe<Document>): boolean {
+    return R.isNonNullish(value?.parent);
 }
